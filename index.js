4069be95499042ccbd4b8e5ba3ca728271919f45
Orie Steele <orie@transmute.industries>
2022-03-02 08:45:39 -0600

    chore: test

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const fs = require("fs");
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`🇺🇦 Hello ${nameToGreet}!`);
 
-  const howToGreet = core.getInput("how-to-greet");
+  const howToGreet = core.getInput("how-to-greet"); // a path
   console.log(howToGreet);
+
+  const file = fs.readFileSync(howToGreet);
+  console.log(file.toString());
 
   const time = new Date().toTimeString();
   core.setOutput("time", time);
